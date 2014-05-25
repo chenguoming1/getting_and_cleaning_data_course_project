a5ddda3554f9d97b5db94a40251295407baa635a
chenguoming1@gmail.com <aungbn@streammed.com>
2014-05-25 22:19:46 +0800

    minor changes

--- get_file_path.R
+++ get_file_path.R
@@ -1,8 +1,3 @@
 get_file_path<-function(directory, id) {
-  if (is.numeric(id)) {
-    id<-sprintf("%03d", id)
-  }else {
-    id<-sprintf("%03s", id)
-  }
   fpath <- normalizePath(file.path(directory, id))
 }
